1bddc6348bcd73af21c711fd8ed30f04f1cf0e63
RenzouM <renzoumgmail.com>
2024-02-19 17:02:29 -0300

    asd

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -14,7 +14,7 @@ function NavBar() {
     <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link
-          href="#"
+          to="/"
           class="flex items-center space-x-3 rtl:space-x-reverse">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/135px-Flag_of_Argentina.svg.png"
